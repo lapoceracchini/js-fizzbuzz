1a0301cf38b9249f859c254874d87ec98c30b98e
lapoceracchini <lapoceracchini@gmail.com>
2023-03-27 01:27:26 +0200

    fine

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+CONSEGNA:
+
 Scrivi un programma che stampi in console i numeri da 1 a 100,
 ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+SOLUZIONE:
+
+1. creare un ciclo 1-100
+
+2. selezionare i numeri multipli sia di 5 che di 3
+
+3. selezionare i numeri multipli solo di 3
+
+4. selezionare i numeri multipli solo di 5
---
+++ script.js
@@ -0,0 +1,22 @@
+const container = document.querySelector('.container');
+
+for(let i = 1; i <= 100; i++){
+
+  const box = document.createElement('div');
+  box.className = 'box';
+
+  let output = i;
+
+  if(!(i % 15)){
+    output = 'FizzBuzz';
+    box.classList.add('fizzbuzz');
+  }else if(!(i % 3)){
+    output = 'Fizz'
+    box.classList.add('fizz');
+  }else if(!(i % 5)){
+    output = 'Buzz'
+    box.classList.add('buzz');
+  }
+  box.append(output);
+  container.append(box);
+}
